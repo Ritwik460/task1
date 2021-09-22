828d7c941184e98dd74ddbbf687305f68fae43ea
Ritwik460 <91183749+Ritwik460@users.noreply.github.com>
2021-09-22 17:39:06 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="test-center"> Insert data using Ajax and jquary</h1>
+        <br>
+        <div class="col-lg-8 m-auto">
+            <form id="myform" action="insertdata.php" method="POST">
+                <div class="form-group">
+                    <label>Username :</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Password :</label>
+                    <input type="text" name="password" class="form-control">
+                </div>
+                <input type="submit" name="submit" value="submit" id="submit" class="btn btn-success">
+            </form>
+        </div>
+    </div>
+    <script src="script.js">
+        $(document).ready(function() {
+
+            var form = $('#myform');
+
+            $('#submit').click(function() {
+
+                $.ajax({
+                    url: form.attr("action"),
+                    type: 'POST',
+                    data: $("#myform input").serialize(),
+
+                    success: function(data) {
+                        console.log(data);
+                    }
+
+                });
+            });
+
+        });
+    </script>
+</body>
+
+</html>
